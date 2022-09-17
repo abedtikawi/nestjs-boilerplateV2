c7bbdf5225fc6de2005b2c601f00c6c1a8de6d61
AbedTikkawi <abed.tikkawi@360mea.com>
2022-09-17 22:30:42 +0300

    displaced swagger into middleware

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import { NestFactory } from '@nestjs/core';
 import { middleware } from './app.middleware';
 import { AppConfigService } from 'src/shared/config.service';
 import { serveSwagger } from './documentation/serve-swagger';
+import { DEV_ENV } from './common';
 
 async function bootstrap(): Promise<string> {
   const app = await NestFactory.create<NestExpressApplication>(
@@ -16,19 +17,10 @@ async function bootstrap(): Promise<string> {
     { cors: true },
   );
 
-  await app.init();
-
   const appConfig = app.get(AppConfigService);
 
-  await middleware(app);
+  await middleware(app, appConfig);
 
-  if (['development', 'staging', 'testing'].includes(appConfig.env)) {
-    await serveSwagger(app, appConfig.name);
-    Logger.log(
-      `Serving Documentation: http://localhost:${appConfig.port}/api`,
-      'Swagger',
-    );
-  }
   await app.listen(appConfig.port);
 
   return await app.getUrl();
@@ -36,6 +28,7 @@ async function bootstrap(): Promise<string> {
 (async (): Promise<void> => {
   try {
     const url = await bootstrap();
+
     Logger.log(`Application running on: ${url}`, 'Bootstrap');
   } catch (error) {
     Logger.error(error.message, 'Bootstrap');
