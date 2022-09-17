9b73015bb8273d7d40b421a510c215389ccec0f3
AbedTikkawi <abed.tikkawi@360mea.com>
2022-09-17 22:32:11 +0300

    improved swagger createDocument

--- src/documentation/serve-swagger.ts
+++ src/documentation/serve-swagger.ts
@@ -1,22 +1,27 @@
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, Logger } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppConfigService } from 'src/shared/config.service';
 
 export async function serveSwagger(
   app: INestApplication,
-  applicationName: string,
+  applicationConfiguration: AppConfigService,
 ): Promise<void> {
+  const { port, name, baseURL } = applicationConfiguration;
+
   const config = new DocumentBuilder()
     .setTitle(` Microservice`)
-    .setTitle(`${applicationName} Microservice`)
-    .setDescription(`${applicationName} Microservice documentation `)
-    .setVersion('1.0') // this should be removed, create space for other versions
+    .setTitle(`${name} Microservice`)
+    .setDescription(`${name} Microservice documentation `)
+    .setVersion('1.0')
     .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
+  const path = 'api';
   SwaggerModule.setup('api', app, document, {
     swaggerOptions: {
       persistAuthorization: true,
     },
   });
+  Logger.log(`Serving Documentation on ${baseURL}:${port}/${path}`, 'Swagger');
 }
