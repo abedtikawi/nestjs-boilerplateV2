183fc7536f0413ebdd00807355ca6d7ec0633503
AbedTikkawi <abed.tikkawi@360mea.com>
2022-09-20 21:50:43 +0300

    imported mysql module aside from shared module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { SharedModule } from './shared/shared.module';
 import { UsersModule } from './components/users/users.module';
+import { MySQLModule } from './database/mysql/mysql.module';
 
 @Module({
-  imports: [SharedModule, UsersModule],
+  imports: [SharedModule, UsersModule, MySQLModule],
   controllers: [AppController],
   providers: [AppService],
 })
